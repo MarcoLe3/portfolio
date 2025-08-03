283f4484b14063d5bddbf4945635841fbe9b9021
Marco Lee <marcole32k3@gmail.com>
2025-08-02 17:58:19 -0700

    e

--- components/cards/Nav.jsx
+++ components/cards/Nav.jsx
@@ -4,10 +4,11 @@ import Link from 'next/link';
 const navigationData = {
   main: [{ title: "Home", url: "/" }],
   Exprience: [
-    { title: "Eazifunds", url: "/Eazifunds" },
-    { title: "Popper", url: "#margeta" },
-    { title: "SF Living Wages Coalition", url: "#propertyguru" },
-    { title: "City of San Francisco", url: "#micro-interactions" },
+    { title: "Cal Poly Digital Transformation Hub, AWS", url: "/Contact_Page" },
+    { title: "Eazifunds", url: "/Contact_Page" },
+    { title: "Popper", url: "/Contact_Page" },
+    { title: "SF Living Wages Coalition", url: "/Contact_Page" },
+    { title: "City of San Francisco", url: "/Contact_Page" },
   ],
   contact: [
     { title: "Mail", url: "mailto:marcole32k3@gmail.com" },
