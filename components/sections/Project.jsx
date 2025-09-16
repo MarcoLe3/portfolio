0b23391297cd83b1c8a041596d8cd02caa94edf2
Marco Lee <marcole32k3@gmail.com>
2025-09-16 14:33:19 -0700

    qwe

--- components/sections/Project.jsx
+++ components/sections/Project.jsx
@@ -4,7 +4,7 @@ import SlideShow from "../button/slide";
 const Projects = () => {
   return (
     <section className="w-full py-12 bg-white flex flex-col gap-3 h-[110vh]">
-      <h1 className="text-[60px] font-medium text-center animate-fade-in bg-gradient-to-b from-[#38434F] to-[#788C9C] text-transparent bg-clip-text">
+      <h1 className="text-[60px] font-medium text-center bg-gradient-to-b from-[#38434F] to-[#788C9C] text-transparent bg-clip-text">
         Projects
       </h1>
 
