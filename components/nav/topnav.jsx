d71d978b15f1d35c4ca6d60aed4a9eb7d303dade
Marco Lee <marcole32k3@gmail.com>
2025-09-17 12:28:16 -0700

    testing husky

--- components/nav/topnav.jsx
+++ components/nav/topnav.jsx
@@ -5,7 +5,7 @@ export default function TopHomeNav() {
     <nav className="fixed w-full bg-black text-center bg-opacity-70 z-50 p-y-4">
       <Link
         href="/"
-        className="text-gray-400 text-[13px] font-rubik px-4 py-2 font-thin hover:text-white transition-colors duration-300"
+        className="text-gray-400 text-[15px] font-rubik px-4 py-2 font-thin hover:text-white transition-colors duration-300"
       >
         Home
       </Link>
