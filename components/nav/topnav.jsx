13595af5c6c35ce8a1d77744fbbc4553bebc7ee0
Marco Lee <marcole32k3@gmail.com>
2025-09-24 14:33:14 -0700

    font

--- components/nav/topnav.jsx
+++ components/nav/topnav.jsx
@@ -2,13 +2,17 @@ import Link from 'next/link';
 
 export default function TopHomeNav() {
   return (
-    <nav className="fixed w-full text-center bg-opacity-70 z-50 p-y-4">
+    <nav className="w-full text-center z-50 p-y-4 text-black text-[17px] font-rubik px-4 py-2 font-thin hover:text-gray-700 transition-colors duration-300">
       <Link
         href="/"
-        className="text-black text-[17px] font-rubik px-4 py-2 font-thin hover:text-gray-700 transition-colors duration-300"
       >
-        Home
+        Home 
       </Link>
+      {/* <Link
+        href="https://www.linkedin.com/in/marco-lee-1461a0254/"
+      >
+        Linkedln
+      </Link> */}
     </nav>
   );
 }
--- components/sections/hero.jsx
+++ components/sections/hero.jsx
@@ -5,29 +5,28 @@ function Hero() {
   return (
      <div className="
         w-full 
-        min-h-[70vh] sm:min-h-[80vh] md:min-h-screen 
-        flex items-center justify-center px-4 
+        min-h-[70vh]
+        flex justify-center px-4 
         text-[#0D1B2A]  text-black bg-white
       ">
       <div className="flex flex-col items-center text-center max-w-[90vw] sm:max-w-[80vw] md:max-w-[900px]">
-        {/* Name */}
         <Image
           src="/image/me.png"
-          width={300}
-          height={300}
+          width={350}
+          height={350}
           loading='lazy'
         />
-        <h1 className="text-[10vw] sm:text-[8vw] md:text-[6vw] lg:text-[5vw] leading-none opacity-0 animate-fade-in font-medium">
+        <h1 className="text-[10vw] sm:text-[10vw] md:text-[10vw] lg:text-[6.5vw] leading-none opacity-0 animate-fade-in font-semibold tracking-tighter">
           MARCO LEE
         </h1>
 
         {/* Tagline */}
-        <p className="text-[4.5vw] sm:text-[3.5vw] md:text-[2.5vw] lg:text-[1.6vw] leading-none -mt-1 mb-6 opacity-0 animate-fade-in-delay font-medium">
-          FRONTEND DEVELOPER PORTFOLIO
+        <p className="text-[4.5vw] sm:text-[3.5vw] md:text-[2.5vw] lg:text-[1.6vw] leading-none opacity-0 animate-fade-in-delay">
+          FRONTEND DEVELOPER PORTFOLIO  
         </p>
 
         {/* Description */}
-        <p className="text-[4vw] sm:text-[3vw] md:text-[2vw] lg:text-[1.5vw] leading-snug -mt-2 text-gray-500 opacity-0 animate-fade-in-delay font-medium font-instrument">
+        <p className="text-[4.5vw] sm:text-[3.5vw] md:text-[2.5vw] lg:text-[1.6vw] leading-snug text-black opacity-0 animate-fade-in-delay font-instrument">
           Based in San Francisco. Driven by a passion for building refined,{" "}
           <span className="bg-gradient-to-r from-blue-400 to-blue-700 text-transparent bg-clip-text">
             intuitive
